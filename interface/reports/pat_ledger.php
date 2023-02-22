0704f46da744044abaf61c944f8558c2c6f0bcea
juggernautsei <sherwingaddis@gmail.com>
2023-02-22 16:26:01 -0500

    fixing report print out

--- interface/reports/pat_ledger.php
+++ interface/reports/pat_ledger.php
@@ -677,8 +677,8 @@ if ($_REQUEST['form_refresh'] || $_REQUEST['form_csvexport']) {
         }
 
         $facility = $facilityService->getById($form_facility);
+        echo "PID" . $form_patient;
         $patient = sqlQuery("SELECT * from patient_data WHERE pid = ?", array($form_patient));
-        var_dump($patient);
         $pat_dob = $patient['DOB'] ?? null;
         $pat_name = ($patient['fname'] ?? '') . ' ' . ($patient['lname'] ?? '');
 
