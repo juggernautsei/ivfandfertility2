03d94d91704f12b048997a3e00a9e82323a25c92
Sherwin <sherwingaddis@gmail.com>
2023-03-14 13:48:23 -0400

    more changes.

--- interface/reports/pat_ledger.php
+++ interface/reports/pat_ledger.php
@@ -751,11 +751,11 @@ if ($_REQUEST['form_refresh'] || $_REQUEST['form_csvexport']) {
                         }
                         if ($form_facility == 5) {
                             $title = User_Id_Look(13);
-                            $title .= "\n\rFacility NPI: " . $facility['facility_npi'];
+                            $title .= "\n\rNPI: " . $facility['facility_npi'];
                         }
                         if ($form_facility == 4) {
                             $title = User_Id_Look(13);
-                            $title .= "\r\nFacility NPI: " . $facility['facility_npi'];
+                            $title .= "\r\nNPI: " . $facility['facility_npi'];
                         }
                         ?>
                         <?php echo nl2br(htmlentities($title)); ?>
