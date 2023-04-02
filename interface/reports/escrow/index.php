a85ad3e6393c5d009bc7ab19053cc4600032bcb0
Sherwin <sherwingaddis@gmail.com>
2023-04-02 07:25:00 -0400

    Escrow report build refining query

--- interface/reports/escrow/index.php
+++ interface/reports/escrow/index.php
@@ -46,7 +46,7 @@
         <div class="row">
             <div class="col-sm-12">
                 <?php
-                    require_once("../../../patient_file/summary/dashboard_header.php")
+                    require_once dirname(__FILE__, 3) . "/patient_file/summary/dashboard_header.php";
                 ?>
             </div>
         </div>
