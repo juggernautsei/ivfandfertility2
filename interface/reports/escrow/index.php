82fed1bff5e6982d927e14891b3495c08830b91a
juggernautsei <sherwingaddis@gmail.com>
2023-04-06 16:35:25 -0400

    Escrow report build refining query

--- interface/reports/escrow/index.php
+++ interface/reports/escrow/index.php
@@ -76,14 +76,14 @@
                         $sumOfPayments = 0;
                         foreach ($listPayments as $payment) {
 
-                            if ($lpayment == $payment) {
-                                echo "<tr><td></td><td></td><td></td><td>" . $lpayment['pay_amount'] + $payment['pay_amount'] . "</td></tr>";
+                            //if ($lpayment == $payment) {
+                              //  echo "<tr><td></td><td></td><td></td><td>" . $lpayment['pay_amount'] + $payment['pay_amount'] . "</td></tr>";
 
-                            }
-                            //echo "<tr>";
-                            //echo "<td></td><td>" . substr($payment['post_time'], 0, -9) . "</td><td>" . $payment['encounter'] . "</td><td>" . $payment['pay_amount'] . "</td>";
-                            //echo "</tr>";
-                                $lpayment = $payment;
+                            //}
+                            echo "<tr>";
+                            echo "<td></td><td>" . substr($payment['post_time'], 0, -9) . "</td><td>" . $payment['encounter'] . "</td><td>" . $payment['pay_amount'] . "</td>";
+                            echo "</tr>";
+                                //$lpayment = $payment;
                             $sumOfPayments = $sumOfPayments + $payment['pay_amount'];
                         }
                         echo "<tr>";
