749f2dd252fca4f785415d32413fa89454842cad
juggernautsei <sherwingaddis@gmail.com>
2023-04-19 14:57:13 -0400

    looking for errors

--- interface/billing/edit_payment.php
+++ interface/billing/edit_payment.php
@@ -19,6 +19,9 @@
  * @license https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 require_once("../globals.php");
 require_once("$srcdir/auth.inc");
