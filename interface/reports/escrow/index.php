b9711e5373079709784d0714951b43f290d34b6e
juggernautsei <sherwingaddis@gmail.com>
2023-11-13 07:35:01 -0500

    building refund process

--- interface/reports/escrow/index.php
+++ interface/reports/escrow/index.php
@@ -118,7 +118,7 @@
     $refund = document.getElementById('refund');
     $refund.addEventListener('click', function () {
         let url = '<?php echo $GLOBALS['webroot'] . '/interface/reports/escrow/refund.php'?>';
-        dlgopen(url, 'pop-profile', 'modal-sm', 450, '', '', {
+        dlgopen(url, '_blank', 'modal-sm', 250, '', '', {
             allowDrag: true,
             allowResize: true,
             buttons: [
