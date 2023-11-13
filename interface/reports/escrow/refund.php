e96d5ec40c75d87502a43f7cb4d9518be5396621
juggernautsei <sherwingaddis@gmail.com>
2023-11-13 00:00:56 -0500

    building refund process

--- interface/reports/escrow/refund.php
+++ interface/reports/escrow/refund.php
@@ -7,7 +7,7 @@
  * Copyright (c) 2023.
  */
 require_once dirname(__FILE__, 3) . "/globals.php";
-
+use OpenEMR\Core\Header;
 ?>
 <!doctype html>
 <html>
@@ -17,6 +17,7 @@ require_once dirname(__FILE__, 3) . "/globals.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <?php Header::setupHeader(); ?>
 </head>
 <body>
 <div class="container">
@@ -24,7 +25,6 @@ require_once dirname(__FILE__, 3) . "/globals.php";
         <div class="col-12">
             <h3><?php echo xlt("Refund") ?></h3>
             <form class="form-group" method="post" action="refund.php">
-                <label for="amount"><?php echo xlt("Amount") ?></label>
                 <label for="amount"><?php echo xlt("Amount") ?>
                     <input type="text" class="form-control" name="amount" >
                 </label>
