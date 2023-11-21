e50a9abe1749cc13751b5539c1bb0e764395f5ab
juggernautsei <sherwingaddis@gmail.com>
2023-11-21 11:00:19 -0500

    Refund build - save TS

--- interface/reports/escrow/Escrow.php
+++ interface/reports/escrow/Escrow.php
@@ -80,7 +80,10 @@ namespace OpenEMR\Escrow;
                     $_SESSION['authUserID'],
                     0,
                     $this->reference,
-                    '2023-11-01', '2023-11-08',   '-6350.00',  '2023-11-09 21:31:03', '2023-11-09 21:28:12', '0.00',         'clinic',     'Balance Refund', 'refund_balance', '2023-11-09',    26268,       'check_payment'
+                    $this->checkDate,
+                    $this->checkDate,
+                    $this->payTotal,
+                    '2023-11-09 21:31:03', '2023-11-09 21:28:12', '0.00',         'clinic',     'Balance Refund', 'refund_balance', '2023-11-09',    26268,       'check_payment'
                     ]);
             } catch (\Exception $e) {
                 return $e->getMessage();
